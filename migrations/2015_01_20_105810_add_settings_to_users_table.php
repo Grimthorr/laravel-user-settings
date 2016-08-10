848e92bf3279c59db63ca8c678293aa245552fce
Johannes Schobel <johannesschobel@users.noreply.github.com>
2016-08-10 11:21:25 +0200

    switch from JSON to TEXT
    because of backwards compatibility move back from JSON to TEXT

--- migrations/2015_01_20_105810_add_settings_to_users_table.php
+++ migrations/2015_01_20_105810_add_settings_to_users_table.php
@@ -14,7 +14,7 @@ class AddSettingsToUsersTable extends Migration {
 	{
 		Schema::table('users', function(Blueprint $table)
 		{
-			$table->json('settings');
+			$table->text('settings');
 		});
 	}
 
